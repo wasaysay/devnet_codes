49bc374ff0bec8df4b3147e623cb10e3267361b8
wasaysay <wasaysay@163.com>
2022-08-05 16:54:22 +0800

    Add netmiko examples of send_command_timing

---
+++ netmiko/netmiko_try_send_command_timming.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+
+key_file = r"C:\Users\Eden\.ssh\openssh"
+
+hillstone1 = {
+    "device_type": "hillstone",
+    "host": "10.0.0.253",
+    "port": "22",
+    "username": "hillstoneapi",
+    "use_keys": True,
+    "key_file": key_file,
+    "session_log": "output.txt",
+    # Disable algorithms for Hillstone devices.. the following two are not supported in Hillstone SSHD (debug sshd).
+    # See https://www.5axxw.com/questions/content/2l1peh
+    # This option can be found in base_connection.py
+    "disabled_algorithms": dict(pubkeys=["rsa-sha2-512","rsa-sha2-256"]),
+}
+
+with ConnectHandler(**hillstone1) as net_connect:
+    # When using send_command(), there will be error "Read Timeout" and "Pattern not detected".
+    # Since send_command() is pattern-based.
+    # output = net_connect.send_command("show tech-support toconsole")
+    # send_command_timming() is working as expected.
+    output = net_connect.send_command_timing("show tech-support toconsole")
+
+print()
+print(output)
+print()
